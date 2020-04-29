8cc776d6846b3676d2d5b3b60c9926a84b7c27c6
Yossi Rafelson <yossi.rafelson@gmail.com>
2020-04-28 18:34:42 -0700

    Remove old omer before moving new folder into place

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 set -x #echo on
 curl -L -O https://github.com/Yossi/omer/archive/pythonanywhere.zip
 unzip pythonanywhere.zip
+rm pythonanywhere.zip
+rm -rf omer
 mv omer-pythonanywhere omer
 mv omer/deploy.sh deploy.sh
-rm pythonanywhere.zip
 cd omer
 source virtualenvwrapper.sh
 workon omer
