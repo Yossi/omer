a6981ab06a7a9af2daa11a8588bc5644c3d12b84
Yossi Rafelson <yossi.rafelson@gmail.com>
2020-05-01 18:25:19 -0700

    deploy.sh now announces what it's doing

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,23 @@
+echo Downloading https://github.com/Yossi/omer/archive/pythonanywhere.zip
 curl -L -O https://github.com/Yossi/omer/archive/pythonanywhere.zip
+echo Unzipping...
 unzip pythonanywhere.zip
 rm pythonanywhere.zip
+echo Deleted pythonanywhere.zip
 rm -rf omer
 mv omer-pythonanywhere omer
+echo Updated omer/
 mv omer/deploy.sh deploy.sh
+echo Updated deploy.sh
 cd omer
 source virtualenvwrapper.sh
 workon omer
+echo Virtualenv omer activated
 pip install -U -r requirements.txt
+echo Attempting to list outdated packages
 pip list --outdated --format=columns
-rm -rf .cache
-rm omer/.gitignore
-rm omer/requirements.txt
+echo Remove some junk
+rm -rf ../.cache
+rm .gitignore
+rm requirements.txt
+echo Done. Now go reload site from the Web tab.
