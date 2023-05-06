abec8989b4ac7c9b44ec8fad679a9e5414ed9204
Yossi <yossi.rafelson@gmail.com>
2023-05-05 18:31:51 -0700

    Re-enable numba now that we just install each requirement one at a time

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+# manually set up a virtualenv on pythonanywhere
+# mkvirtualenv omer --python=/usr/bin/python3.10
+# also point the web tab at this after
+
 echo Downloading https://github.com/Yossi/omer/archive/pythonanywhere.zip
 curl -L -O https://github.com/Yossi/omer/archive/pythonanywhere.zip
 echo Unzipping...
@@ -14,7 +18,16 @@ cd omer
 source virtualenvwrapper.sh
 workon omer
 echo Virtualenv omer activated
-pip install -U --upgrade-strategy only-if-needed -r requirements.txt
+
+echo Install requirements one by one to not blow up storage
+
+while read -r line || [[ -n "$line" ]]; do
+    if [[ $line != \#* ]]; then
+        pip install -U --upgrade-strategy only-if-needed "$line"
+    fi
+done < requirements.txt
+
+# pip install -U --upgrade-strategy only-if-needed -r requirements.txt
 echo Attempting to list outdated packages:
 pip list --outdated
 rm -rf ../.cache
